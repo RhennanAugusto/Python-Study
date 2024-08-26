bbb011c7dd53d1dcded7bf2edfa096b9046d1d37
RhennanAugusto <rhennanaugusto23@gmail.com>
2024-08-26 09:03:49 -0300

    changes

--- split_join_strip.py
+++ split_join_strip.py
@@ -1,3 +1,13 @@
-frase = "O modelo é inválido"
-lista_palavras = frase.split()
+frase = " Olha só que , coisa interessante "
+lista_palavras_cruas = frase.split(',')
+
+lista_palavras = []
+for i, frase in enumerate(lista_palavras_cruas):
+    lista_palavras.append(lista_palavras_cruas[i].strip()) #tira os espaços da frase
+
+print(lista_palavras_cruas)
 print(lista_palavras)
+
+
+frases_unidas = '-'.join(lista_palavras) # metódo de como fazer a junção de string com base no que foi passado entre aspas
+print(frases_unidas)
